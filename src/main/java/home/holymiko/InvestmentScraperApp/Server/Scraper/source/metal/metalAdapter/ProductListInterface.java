2724615c25a8c3e18795659a337918661d991258
stepaj50 <stepaj50@fit.cvut.cz>
2023-02-11 20:18:11 +0100

    Merge fix

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/Scraper/source/metal/metalAdapter/ProductListInterface.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Scraper/source/metal/metalAdapter/ProductListInterface.java
@@ -3,10 +3,12 @@ package home.holymiko.InvestmentScraperApp.Server.Scraper.source.metal.metalAdap
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import home.holymiko.InvestmentScraperApp.Server.Core.exception.ResourceNotFoundException;
 import home.holymiko.InvestmentScraperApp.Server.Type.Entity.Link;
 import home.holymiko.InvestmentScraperApp.Server.Type.Enum.Dealer;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +18,9 @@ import org.slf4j.LoggerFactory;
 public interface ProductListInterface {
     Logger LOGGER = LoggerFactory.getLogger(ProductListInterface.class);
 
+    default HtmlPage getPage(final String link) throws ResourceNotFoundException {
+        throw new NotImplementedException("Method haven't been implemented yet");
+    }
     default List<Link> scrapAllLinksFromProductLists() {
         throw new NotImplementedException("Method haven't been implemented yet");
     }
@@ -32,6 +37,18 @@ public interface ProductListInterface {
                 .map(
                         this::scrapLink
                 ).collect(Collectors.toList());
+    }
+
+    default List<Link> scrapAllLinksFromProductListUtil(List<String> urls) {
+        List<Link> elements = new ArrayList<>();
+        for (String url:urls) {
+            try {
+                elements.addAll(scrapLinksFromProductList(getPage(url)));
+            } catch (ResourceNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return elements;
     }
 
     default List<HtmlElement> scrapProductList(HtmlPage page) {
