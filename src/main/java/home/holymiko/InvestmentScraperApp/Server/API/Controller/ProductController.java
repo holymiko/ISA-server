d9ca36db6f164b2c44371f87ee2ba0c954bc49e0
holymiko <holymiko@cvut.cz>
2023-03-15 12:02:55 +0100

    ProductControllerTest byId

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/ProductController.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/ProductController.java
@@ -1,6 +1,7 @@
 package home.holymiko.InvestmentScraperApp.Server.API.Controller;
 
 import home.holymiko.InvestmentScraperApp.Server.Core.annotation.ResourceNotFound;
+import home.holymiko.InvestmentScraperApp.Server.Core.exception.ResourceNotFoundException;
 import home.holymiko.InvestmentScraperApp.Server.Type.DTO.advanced.ProductDTO_AllPrices;
 import home.holymiko.InvestmentScraperApp.Server.Type.DTO.advanced.ProductDTO_LatestPrices;
 import home.holymiko.InvestmentScraperApp.Server.Type.Enum.Metal;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -27,18 +27,16 @@ public class ProductController {
 
     /////// GET
 
-    @ResourceNotFound
     @GetMapping("/id/{id}")
-    public Optional<ProductDTO_AllPrices> byId(@PathVariable long id) {
+    public ProductDTO_AllPrices byId(@PathVariable long id) {
         System.out.println("Get product by Id");
         return productService.findByIdAsDTOAllPrices(id);
     }
 
     /////// GET DTO
 
-    @ResourceNotFound
     @GetMapping("/dto/id/{id}")
-    public Optional<ProductDTO_LatestPrices> byIdAsDTO(@PathVariable long id) {
+    public ProductDTO_LatestPrices byIdAsDTO(@PathVariable long id) {
         System.out.println("Get by Id "+id+" as Product DTO");
         return productService.findByIdAsDTO(id);
     }
@@ -57,7 +55,7 @@ public class ProductController {
             case "silver" -> this.productService.findByMetalAsDTO(Metal.SILVER);
             case "platinum" -> this.productService.findByMetalAsDTO(Metal.PLATINUM);
             case "palladium" -> this.productService.findByMetalAsDTO(Metal.PALLADIUM);
-            default -> throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            default -> throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         };
     }
 
