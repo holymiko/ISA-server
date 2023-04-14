557dc5e803d1ac5efeca59ddd53782a4a12b7a0e
holymiko <holymiko@cvut.cz>
2023-04-14 17:09:19 +0200

    User POST mock

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/UserController.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/UserController.java
@@ -1,5 +1,6 @@
 package home.holymiko.InvestmentScraperApp.Server.API.Controller;
 
+import home.holymiko.InvestmentScraperApp.Server.Type.DTO.create.UserCreateDTO;
 import home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple.UserDTO;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -17,6 +18,13 @@ public class UserController {
     @GetMapping({"/me", "/me/"})
     public UserDTO getAssignedClient() {
         return new UserDTO("Tomas", "Dummy", "User", "tomas_dummy_user@world.com", 45665466);
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody UserCreateDTO userCreateDTO) {
+        LOGGER.info("User save");
+//        this.accountService.save(accountCreateDTO);
+//        LOGGER.info("User save success");
     }
 
 }
