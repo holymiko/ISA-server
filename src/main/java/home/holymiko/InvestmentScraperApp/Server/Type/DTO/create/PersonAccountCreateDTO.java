0dd525262931ce003283ccd5024d492d24ec78b3
holymiko <holymiko@cvut.cz>
2023-04-24 12:07:58 +0200

    PUT Person fix

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/DTO/create/PersonAccountCreateDTO.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/DTO/create/PersonAccountCreateDTO.java
@@ -9,7 +9,7 @@ public class PersonAccountCreateDTO extends PersonDTO {
     // Credentials & Role
     private final AccountCreateDTO account;
 
-    public PersonAccountCreateDTO(Long id, String firstName, String middleName, String lastName, String email, int phone, AccountCreateDTO account) {
+    public PersonAccountCreateDTO(Long id, String firstName, String middleName, String lastName, String email, Long phone, AccountCreateDTO account) {
         super(id, firstName, middleName, lastName, email, phone);
         this.account = account;
     }
