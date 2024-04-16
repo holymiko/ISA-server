21eb2b8c6070f2d6307f519f89125791a9ac380d
holymiko <holymiko@cvut.cz>
2024-04-16 14:56:09 +0200

    request.sql

--- request.sql
+++ request.sql
@@ -8,3 +8,10 @@ DELETE FROM price_pair where id in
       where
               l.dealer = 0 and
               p.date_time <= '2023-11-23 23:59:08.172019')
+
+
+-- Synchronize ID generating after DB copy/restore
+SELECT nextval('hibernate_sequence');
+SELECT MAX(id) FROM price;
+SELECT setval('hibernate_sequence', (SELECT MAX(id) FROM price)+1);
+-- If you have problem to import into empty DB, try set sequence to 0
