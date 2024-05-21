479259bf652a1f020aa71617e3ae12304081ba37
holymiko <holymiko@cvut.cz>
2024-05-21 14:52:30 +0200

    Disable security for dev profile

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/Core/config/SecurityConfig.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Core/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import home.holymiko.InvestmentScraperApp.Server.Service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Profile("!dev")
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
---
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Core/config/SecurityConfigDev.java
@@ -0,0 +1,27 @@
+package home.holymiko.InvestmentScraperApp.Server.Core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration
+public class SecurityConfigDev {
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(req -> req.anyRequest().permitAll())
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .build();
+    }
+}
