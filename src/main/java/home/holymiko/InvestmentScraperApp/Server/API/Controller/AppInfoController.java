8050edaa5103eaea24776e6bd813566843c62c79
holymiko <holymiko@cvut.cz>
2024-06-03 00:28:33 +0200

    StatsDTO

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/AppInfoController.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/AppInfoController.java
@@ -1,6 +1,7 @@
 package home.holymiko.InvestmentScraperApp.Server.API.Controller;
 
 import home.holymiko.InvestmentScraperApp.Server.Service.*;
+import home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple.StatsDTO;
 import home.holymiko.InvestmentScraperApp.Server.Type.Enum.Dealer;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -26,21 +27,23 @@ public class AppInfoController extends BaseController {
     private final StockGrahamService stockGrahamService;
 
     @GetMapping("/stats")
-    public String getDatabaseStatistics() {
-        return "rates: " + rateService.count() +
-                "\nproducts: " + productService.countByParams(null, null, null, null, null, null, null) +
-                "\nprice pairs: " + priceService.countPricePairs() +
-                "\nprice pairs history: " + priceService.countPricePairsHistory() +
-                "\ntickers: " + tickerService.countByParams(null) +
-                "\nstocks: " + stockGrahamService.countByParams() +
-                "\nlinks: " + linkService.countByParams(null) +
-                "\n - aurumpro.cz: " + linkService.countByParams(Dealer.AURUM_PRO) +
-                "\n - bessergold.cz: " + linkService.countByParams(Dealer.BESSERGOLD_CZ) +
-                "\n - bessergold.de: " + linkService.countByParams(Dealer.BESSERGOLD_DE) +
-                "\n - ceskamincovna.cz: " + linkService.countByParams(Dealer.CESKA_MINCOVNA) +
-                "\n - goldasilver.com: " + linkService.countByParams(Dealer.GOLD_A_SILVER) +
-                "\n - silverum.cz: " + linkService.countByParams(Dealer.SILVERUM) +
-                "\n - zlataky.cz: " + linkService.countByParams(Dealer.ZLATAKY);
+    public StatsDTO getDatabaseStatistics() {
+        return new StatsDTO(
+                rateService.count(),
+                productService.countByParams(null, null, null, null, null, null, null),
+                priceService.countPricePairs(),
+                priceService.countPricePairsHistory(),
+                tickerService.countByParams(null),
+                stockGrahamService.countByParams(),
+                linkService.countByParams(null),
+                linkService.countByParams(Dealer.AURUM_PRO),
+                linkService.countByParams(Dealer.BESSERGOLD_CZ),
+                linkService.countByParams(Dealer.BESSERGOLD_DE),
+                linkService.countByParams(Dealer.CESKA_MINCOVNA),
+                linkService.countByParams(Dealer.GOLD_A_SILVER),
+                linkService.countByParams(Dealer.SILVERUM),
+                linkService.countByParams(Dealer.ZLATAKY)
+        );
 
     }
 
---
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/DTO/simple/StatsDTO.java
@@ -0,0 +1,23 @@
+package home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StatsDTO {
+
+    private final Long rates;
+    private final Long products;
+    private final Long pricePairs;
+    private final Long pricePairsHistory;
+    private final Long tickers;
+    private final Long stocks;
+    private final Long links;
+    private final Long linksAurumPro;
+    private final Long linksBessergold;
+    private final Long linksBessergoldDe;
+    private final Long linksCeskaMincovna;
+    private final Long linksGoldASilver;
+    private final Long linksSilverum;
+    private final Long linksZlataky;
+
+}
