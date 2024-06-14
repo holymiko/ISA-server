6b907b409017c4c204e5c3475176aa9c4e7ef582
holymiko <holymiko@cvut.cz>
2024-06-14 13:40:05 +0200

    LinkCountDTO + endpoint

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/LinkController.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/API/Controller/LinkController.java
@@ -1,6 +1,7 @@
 package home.holymiko.InvestmentScraperApp.Server.API.Controller;
 
 import home.holymiko.InvestmentScraperApp.Server.Service.LinkService;
+import home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple.LinkCountDTO;
 import home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple.LinkDTO;
 import home.holymiko.InvestmentScraperApp.Server.Type.Enum.Dealer;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -29,11 +32,20 @@ public class LinkController extends BaseController {
     }
 
     @GetMapping("/count")
-    public Long countByParams(
+    public List<LinkCountDTO> countByParams(
             @RequestParam(required = false) Dealer dealer
     ) {
         LOGGER.info("countByParams {}", dealer);
-        return this.linkService.countByParams(dealer);
+
+        return new ArrayList<>(
+            Arrays.stream(Dealer.values()).map((d) ->
+                new LinkCountDTO(
+                    d,
+                    linkService.countByParams(d),
+                    linkService.countByParams(d, true)
+                )
+            ).toList()
+        );
     }
 
 }
---
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/DTO/simple/LinkCountDTO.java
@@ -0,0 +1,17 @@
+package home.holymiko.InvestmentScraperApp.Server.Type.DTO.simple;
+
+import home.holymiko.InvestmentScraperApp.Server.Type.Enum.Dealer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class LinkCountDTO {
+
+    private final Dealer dealer;
+    private final Long linkCount;
+    private final Long productCount;
+
+}
