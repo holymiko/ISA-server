6e3ebd09979cbcc848ee556be53f7c5355ce50ed
holymiko <holymiko@cvut.cz>
2024-08-06 13:15:24 +0200

    comment

--- src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/Enum/Producer.java
+++ src/main/java/home/holymiko/InvestmentScraperApp/Server/Type/Enum/Producer.java
@@ -1,6 +1,7 @@
 package home.holymiko.InvestmentScraperApp.Server.Type.Enum;
 
 public enum Producer {
+    // DB table product modify checks
     ARGOR_HERAEUS, HERAEUS, MUNZE_OSTERREICH, PERTH_MINT, VALCAMBI,
     PAMP, MEXICO_MINT, ROYAL_CANADIAN_MINT, CHINA_MINT, ROYAL_MINT_UK,
     SOUTH_AFRICAN_MINT, UNITED_STATES_MINT, BAVARIAN_STATE_MINT, POBJOY_MINT,
