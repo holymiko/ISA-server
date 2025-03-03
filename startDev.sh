c80d63df63aacff3d3cd4d6443fc6120dcb773d0
holymiko <holymiko@cvut.cz>
2025-03-03 15:06:33 +0100

    docker-compose client prod/dev

--- startDev.sh
+++ startDev.sh
@@ -1,6 +1,10 @@
 cd "$(dirname "$0")"
 
+# resolves bug, forced rebuild
+sudo docker container remove isa-client-dev-1
 sudo docker container remove isa-server-dev-1
-sudo docker image remove -f isa-server-dev:latest   # resolves bug, forces rebuilding of image
+sudo docker image remove -f isa-client-dev:latest
+sudo docker image remove -f isa-server-dev:latest
+
 sudo docker compose build --no-cache
 sudo docker compose --profile dev up --remove-orphans
